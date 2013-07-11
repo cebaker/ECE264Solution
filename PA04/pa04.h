138f4425aaebc656350e6efcbc000147c05e7425
Cedrick E Baker <cebaker@ecelinux17.ecn.purdue.edu>
2013-07-11 09:41:07 -0400

    Original Files Pulled From

--- PA04/pa04.h
+++ PA04/pa04.h
@@ -1,8 +1,6 @@
 #ifndef pa04_h
 #define pa04_h
 
-#define NUM_VALUES 6
-
 typedef struct snode {
   int index ;
   int value ;
@@ -15,9 +13,9 @@ SparseNode * SparseArray_build(int * indicies, int * values, int length);
 void SparseArray_destroy ( SparseNode * array );
 int SparseArray_getMin ( SparseNode * array );
 int SparseArray_getMax ( SparseNode * array );
-SparseNode * SparseArray_set ( SparseNode * array , int index , int value );
 SparseNode * SparseArray_getNode ( SparseNode * array , int index );
 SparseNode * SparseArray_remove ( SparseNode * array , int index );
+SparseNode * SparseArray_add ( SparseNode * array, int index, int value );
 SparseNode * SparseArray_copy(SparseNode * array);
 SparseNode * SparseArray_merge(SparseNode * array_1, SparseNode * array_2);
 
